59ea4ddedda4ccec2a52c8933691c7ee27408686
Ravirajsinh Parmar <parmarravirajsinhk@gmail.com>
2024-06-24 19:10:44 +0530

    Initial commit
    - create react app with typescript configuration
    - Implemented tailwind css for styling components
    - Added Poppins fonts family for entire application
    - Changed browser tab title and favicon

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App min-h-screen flex justify-center items-center">
+      Library Management System
+    </div>
+  );
+}
+
+export default App;
