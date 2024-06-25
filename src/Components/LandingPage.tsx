f5d6342042502a78990a36525f6731458725a803
Ravirajsinh Parmar <parmarravirajsinhk@gmail.com>
2024-06-26 02:15:17 +0530

    Implemented type writter effect in landing page

--- src/Components/LandingPage.tsx
+++ src/Components/LandingPage.tsx
@@ -1,10 +1,24 @@
 import React from "react";
 import Header from "./Header/Header";
+import Typewriter from "typewriter-effect";
 
 const Landing = () => {
   return (
     <div className="relative">
       <Header />
+
+      <div className="w-screen p-[75px] h-screen fixed flex font-bold justify-center z-[999] items-center text-white text-4xl">
+        <Typewriter
+          options={{
+            strings: [
+              "Books have a unique way of stopping time\n in a particular moment and saying: Let's not forget this",
+              "The more that you read, the more things you will know.\n The more that you learn, the more places you'll go",
+            ],
+            autoStart: true,
+            loop: true,
+          }}
+        />
+      </div>
       <video
         loop
         autoPlay
